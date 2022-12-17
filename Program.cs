9ff4779cbcfbdf7be06b81c98b7eef2463c6dfe6
Maria <newlalamhs@mail.ru>
2022-12-17 14:12:04 +0500

    task62

--- Program.cs
+++ Program.cs
@@ -1,39 +1,44 @@
-﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся 
-двузначных чисел. Напишите программу, которая будет построчно 
-выводить массив, добавляя индексы каждого элемента. Массив размером 2 x 2 x 2
-66(0,0,0) 25(0,1,0)
-34(1,0,0) 41(1,1,0)
-27(0,0,1) 90(0,1,1)
-26(1,0,1) 55(1,1,1)   */
+﻿/* Задача 62. Напишите программу, которая заполнит 
+спирально массив 4 на 4. Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07   */
 
-int[,,] a = new int[2, 2, 2];
+int n = 4;
+int[,] sqareMatrix = new int[n, n];
 
-int[] nums = new int[90];
-for (int i = 0; i < 90; i++)
-    nums[i] = i + 10;
+int temp = 1;
+int i = 0;
+int j = 0;
 
-for (int i = 0; i < a.GetLength(0); i++)
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
 {
-    for (int j = 0; j < a.GetLength(1); j++)
-    {
-        for (int l = 0; l < a.GetLength(2); l++)
-        {
-            int temp = new Random().Next(90);
-
-
-            while (nums[temp] == 0)
-                temp = new Random().Next(90);
-
-            a[i, j, l] = nums[temp];
-            nums[temp] = 0;
-        }
-    }
+    sqareMatrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
 }
 
-for (int i = 0; i < a.GetLength(0); i++)
-    for (int j = 0; j < a.GetLength(1); j++)
+WriteArray(sqareMatrix);
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int l = 0; l < a.GetLength(2); l++)
-            Console.Write($"{a[i, j, l]}({i}, {j}, {l}) ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
+
+            else Console.Write($"{array[i, j]} ");
+        }
         Console.WriteLine();
     }
+}
